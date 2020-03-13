f2b0691e5d15556432ebd7a9dadc714aef1ea92e
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2020-03-14 01:06:27 +0200

    export FormData

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export {IHttpTransport, HttpMethod, TransferProgress} from './interfaces';
 export {HttpTransport} from './http.transport';
 export {RequestError} from './request.error';
+export {FormData} from './utils';
 export * from './symbols'
 import Symbols from './symbols';
 export default Symbols;
