2d757ebe1df1395c92950e6428f81b36183594f5
andrusyshyny <yurij.andrusyshyn@gmail.com>
2020-05-29 12:52:03 +0300

    export redirect error in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export {IHttpTransport, HttpMethod, TransferProgress} from './interfaces';
 export {HttpTransport} from './http.transport';
 export {RequestError} from './request.error';
 export {ConnectionError} from './connection.error';
+export {RedirectError} from './redirect.error';
 export {FormData, URLSearchParams} from './utils';
 export * from './symbols'
 import Symbols from './symbols';
