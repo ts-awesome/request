885e38c1fd9cde073307bbab0249a25568388563
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2020-06-04 06:55:22 +0300

    fixed JSON.stringify for body

--- src/http.transport.base.ts
+++ src/http.transport.base.ts
@@ -464,6 +464,9 @@ function stringify<T>(x: T, space = 2): string {
   const cache = new Set();
   const replacer = (key, value): unknown => {
     if (typeof value === 'object' && value !== null) {
+      if (key === 'body' && typeof value === 'string') {
+        return value;
+      }
       if (key === 'body' && (typeof value.read === 'function' || typeof value.pipe === 'function')) {
         return `<${Object.getPrototypeOf(value)?.constructor?.name || 'stream'} size=${JSON.stringify(value.total ?? value.size)} headers=${JSON.stringify(value.headers ?? value.getHeaders?.())} httpVersion=${JSON.stringify(value.httpVersion)} />`;
       }
